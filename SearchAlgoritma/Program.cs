568cf499085053e7bf15ef1326bbe16df9bbb268
NandaLemon <rizkynandapr@gmail.com>
2022-10-24 16:49:18 +0700

    membuat void Binary

--- SearchAlgoritma/Program.cs
+++ SearchAlgoritma/Program.cs
@@ -28,6 +28,46 @@ namespace SearchAlgoritma
             Console.WriteLine("-----------------");
             Console.WriteLine("Enter Array Element");
             Console.WriteLine("-----------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" +(i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                Console.Write("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at potition" + (mid + 1).ToString());
+                else
+                    Console.WriteLine('\n' + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion :" + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
         }
     }
 }
